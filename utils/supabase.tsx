9d0b9641d7ed54c35a6871a15ddf4cfa3ec08dfe
mark_lariosa <markanthonylariosa3@gmail.com>
2026-02-13 15:31:08 +0800

    pasko

--- utils/supabase.tsx
+++ utils/supabase.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://blwojfybxsllgcwumiav.supabase.co";
 const supabaseKey = "sb_publishable_GIQVmSJ0TmTNVrjk3ZHdIA_y4238CS6";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsd29qZnlieHNsbGdjd3VtaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NzU1NDYsImV4cCI6MjA4NjE1MTU0Nn0.G3WWzDN2sjh46qAVfPU_ME70w-gcREzMXpS1H0gY0mg"
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
